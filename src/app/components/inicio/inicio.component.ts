68753b84478af0247cb4e25e01ea28935aec617d
Catalina Cano <catalinacano08@hotmail.com>
2017-10-28 19:44:51 -0500

    Prueba del nombre

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -40,64 +40,6 @@ export class InicioComponent {
             this.oauthService.logOut();
         }
 
-        loadUserProfile(): void {
-            this
-                .oauthService
-                .loadUserProfile()
-                .then(up => this.userProfile = up);
-
-        }
-
-        get givenName() {
-            var claims = this.oauthService.getIdentityClaims();
-            if (!claims) return null;
-            return claims['given_name'];
-        }
-
-        get familyName() {
-            var claims = this.oauthService.getIdentityClaims();
-            if (!claims) return null;
-            return claims['family_name'];
-        }
-
-        testSilentRefresh() {
-            /*
-             * Tweak config for implicit flow.
-             * This is needed b/c this sample uses both flows
-            */
-            //this.oauthService.clientId = "spa-demo";
-            this.oauthService.oidc = true;
-
-            this
-                .oauthService
-                .silentRefresh()
-                .then(info => console.debug('refresh ok', info))
-                .catch(err => console.error('refresh error', err));
-        }
-
-        set requestAccessToken(value: boolean) {
-            this.oauthService.requestAccessToken = value;
-            localStorage.setItem('requestAccessToken', '' + value);
-        }
-
-        get requestAccessToken() {
-            return this.oauthService.requestAccessToken;
-        }
-
-        get id_token() {
-            return this.oauthService.getIdToken();
-        }
-
-        get access_token() {
-            return this.oauthService.getAccessToken();
-        }
-
-        get id_token_expiration() {
-            return this.oauthService.getIdTokenExpiration();
-        }
-
-        get access_token_expiration() {
-            return this.oauthService.getAccessTokenExpiration();
-        }
+        
 
 }
